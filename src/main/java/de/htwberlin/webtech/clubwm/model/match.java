37412c918f48633b4abf78c367c8ccbfa6db89eb
jmateyka <s0584167@htw-berlin.de>
2025-04-17 17:54:13 +0200

    Hinzufügen 1. Versuch Entity Klasse match.java

---
+++ src/main/java/de/htwberlin/webtech/clubwm/model/match.java
@@ -0,0 +1,51 @@
+package de.htwberlin.webtech.clubwm.model;
+
+public class match {
+
+    private String homeTeam;
+    private String visitorTeam;
+    private int homeScore;
+    private int visitorScore;
+
+    public match() {}
+
+    public match(String homeTeam, String visitorTeam, int homeScore, int visitorScore) {
+        this.homeTeam = homeTeam;
+        this.visitorTeam = visitorTeam;
+        this.homeScore = homeScore;
+        this.visitorScore = visitorScore;
+    }
+
+    // Getter und Setter
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getVisitorTeam() {
+        return visitorTeam;
+    }
+
+    public void setVisitorTeam(String visitorTeam) {
+        this.visitorTeam = visitorTeam;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public void setHomeScore(int homeScore) {
+        this.homeScore = homeScore;
+    }
+
+    public int getVisitorScore() {
+        return visitorScore;
+    }
+
+    public void setVisitorScore(int visitorScore) {
+        this.visitorScore = visitorScore;
+    }
+}
