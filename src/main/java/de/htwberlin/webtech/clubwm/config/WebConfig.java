a36a7673afd02cd25dc4c71b360ae339f7abd8e7
jmateyka <s0584167@htw-berlin.de>
2025-05-11 14:19:17 +0200

    Hinzufügen WebConfig.java

---
+++ src/main/java/de/htwberlin/webtech/clubwm/config/WebConfig.java
@@ -0,0 +1,22 @@
+package de.htwberlin.webtech.clubwm.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://dein-frontend.onrender.com");
+            }
+        };
+    }
+}
