112466252bdee094833958641ce15e393caf7c42
jmateyka <s0584167@htw-berlin.de>
2025-05-24 12:05:02 +0200

    Anpassung Pfad GroupRestController

--- src/main/java/de/htwberlin/webtech/clubwm/web/GroupRestController.java
+++ src/main/java/de/htwberlin/webtech/clubwm/web/GroupRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/groups")
+@RequestMapping("/api/v1/groups")
 public class GroupRestController {
 
     private final GroupService groupService;
