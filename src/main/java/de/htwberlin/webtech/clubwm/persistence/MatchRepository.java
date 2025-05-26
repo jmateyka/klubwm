7d1151babeb1e6e85973e01378a24fc02b6c1dfa
jmateyka <s0584167@htw-berlin.de>
2025-05-26 18:32:00 +0200

    Aufräumen

--- src/main/java/de/htwberlin/webtech/clubwm/persistence/MatchRepository.java
+++ src/main/java/de/htwberlin/webtech/clubwm/persistence/MatchRepository.java
@@ -5,5 +5,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MatchRepository extends JpaRepository<MatchEntity, Long> {
-    // hier evtl. weitere Abfragen einfügen
 }
