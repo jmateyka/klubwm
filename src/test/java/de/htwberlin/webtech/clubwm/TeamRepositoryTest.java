93c111d6a344a83bba50114726c51a5ffb311726
jmateyka <s0584167@htw-berlin.de>
2025-05-26 18:48:51 +0200

    Anpassung Test

--- src/test/java/de/htwberlin/webtech/clubwm/TeamRepositoryTest.java
+++ src/test/java/de/htwberlin/webtech/clubwm/TeamRepositoryTest.java
@@ -4,16 +4,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import de.htwberlin.webtech.clubwm.persistence.TeamEntity;
 import de.htwberlin.webtech.clubwm.persistence.TeamRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @SpringBootTest
+@Transactional
 class TeamRepositoryTest {
 
     @Autowired
     private TeamRepository teamRepository;
+
+    @BeforeEach
+    void setUp() {
+        teamRepository.save(new TeamEntity("Test Team 1", "Country", "Location", "League", 25.0, "1 Mrd. €"));
+        teamRepository.save(new TeamEntity("Test Team 2", "Country", "Location", "League", 27.0, "500 Mio. €"));
+    }
 
     @Test
     void findAllTeams() {
